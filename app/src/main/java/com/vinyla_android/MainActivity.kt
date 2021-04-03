7b839d4f1211ce5b867d51489771b5fbb38c991e
Malibin <nightmare73@naver.com>
2021-04-03 21:54:29 +0900

    [Feat] 바코드 라이브러리 샘플 구동

--- app/src/main/java/com/vinyla_android/MainActivity.kt
+++ app/src/main/java/com/vinyla_android/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.vinyla_android
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdView
+import com.google.zxing.integration.android.IntentIntegrator
 import com.vinyla_android.presentation.utils.loadAd
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +15,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<AdView>(R.id.banner).loadAd()
+        findViewById<Button>(R.id.button).setOnClickListener {
+            IntentIntegrator(this)
+                .setOrientationLocked(false)
+                .setBeepEnabled(false)
+                .initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        Toast.makeText(
+            this,
+            """
+            ${result.barcodeImagePath}
+            ${result.contents}
+            ${result.errorCorrectionLevel}
+            ${result.formatName}
+            ${result.orientation}
+            ${result.originalIntent}
+            ${result.rawBytes}
+        """.trimIndent(),
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
