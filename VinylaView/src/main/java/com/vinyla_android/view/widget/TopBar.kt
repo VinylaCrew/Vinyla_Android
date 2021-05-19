a990bf9941cbd42f20637368311e01438f48da78
malibinYun <nightmare73@naver.com>
2021-05-19 16:27:10 +0900

    [Feat] Top bar 위젯 작성 및 TextField Widget 디자인 수정

---
+++ VinylaView/src/main/java/com/vinyla_android/view/widget/TopBar.kt
@@ -0,0 +1,38 @@
+package com.vinyla_android.view.widget
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.withStyledAttributes
+import com.vinyla_android.view.R
+import com.vinyla_android.view.databinding.WidgetTopBarBinding
+
+class TopBar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val binding = WidgetTopBarBinding.inflate(LayoutInflater.from(context), this)
+
+    var title: String
+        get() = binding.textTitle.text.toString()
+        set(value) {
+            binding.textTitle.text = value
+        }
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.InputTextView) { applyAttributes(this) }
+    }
+
+    private fun applyAttributes(typedArray: TypedArray) {
+        title = typedArray.getString(R.styleable.TopBar_title).orEmpty()
+    }
+
+    fun setOnBackButtonClickListener(listener: ((View) -> Unit)?) {
+        binding.root.setOnClickListener(listener)
+    }
+}
